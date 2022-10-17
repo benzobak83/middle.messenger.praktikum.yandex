a391792e3248553ca375a70f7b9e0a56f2dbe0a5
benzobak83 <vitanaolord@gmail.com>
2022-10-17 18:11:12 +0300

    изменил получение порта через переменное окружение

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config({ path: `process.env` });
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
 
