2159514c583143e7fc7a1651aeef75c00927b6ba
benzobak83 <vitanaolord@gmail.com>
2022-10-17 18:32:20 +0300

    добавил фигурные скобки для читабельности

--- src/utils/mydash/isEmpty.ts
+++ src/utils/mydash/isEmpty.ts
@@ -9,10 +9,16 @@ function isEmpty(value) {
     return typeof value === "string" && isLength();
   }
   function isLength() {
-    if (!value) return false;
-    if (value.length) return true;
+    if (!value) {
+      return false;
+    }
+    if (value.length) {
+      return true;
+    }
     return false;
   }
-  if (isObject() || isString()) return false;
+  if (isObject() || isString()) {
+    return false;
+  }
   return true;
 }
